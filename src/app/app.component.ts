9b38cb0b208b1f50b7a8db3d71abd079ea6a4e93
Marcel <34819524+MarcelCoding@users.noreply.github.com>
2022-08-21 15:21:26 +0200

    Added routing animations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import {Component} from '@angular/core';
+import {routerAnimation} from "../animations/router.animation";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  animations: [routerAnimation],
 })
 export class AppComponent {
 }
